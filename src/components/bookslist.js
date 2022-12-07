f0e282d89f84bd625cbec056e67d0a10164ebd70
AayushmaanRawat <67063637+AayushmaanRawat@users.noreply.github.com>
2022-12-07 10:30:16 +0530

    updated list

--- src/components/bookslist.js
+++ src/components/bookslist.js
@@ -1,135 +1,178 @@
 const List = [
- {
-  title: 'My Book 1',
-  author: 'author-1',
-  date: '29-08-2003',
-  subject: 'Manga'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 3',
-  author: 'author-3',
-  date: '29-08-2007',
-  subject: 'action'
- },
- {
-  title: 'My Book 4',
-  author: 'author-4',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 5',
-  author: 'author-5',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 6',
-  author: 'author-6',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 7',
-  author: 'author-7',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 8',
-  author: 'author-8',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 9',
-  author: 'author-9',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 10',
-  author: 'author-10',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 2',
-  author: 'author-2',
-  date: '29-08-2067',
-  subject: 'action'
- },
- {
-  title: 'My Book 3',
-  author: 'author-3',
-  date: '29-11-2003',
-  subject: 'adventure'
- }
+    {
+        id: '1',
+        title: 'My Book 1',
+        author: 'author-1',
+        date: '29-08-2003',
+        subject: 'Manga'
+    },
+    {
+        id: '2',
+        title: 'My Book 2',
+        author: 'author-2',
+        date: '29-08-2067',
+        subject: 'action'
+    },
+    {
+        id: '3',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '4',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '5',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '6',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '7',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '8',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '9',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '10',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '11',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '12',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '13',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '14',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '15',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '16',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '17',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '18',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '19',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '20',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '21',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '22',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '23',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '24',
+        title: 'My Book 3',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    },
+    {
+        id: '25',
+        title: 'My Book 10',
+        author: 'author-3',
+        date: '29-08-2007',
+        subject: 'action'
+    }
 ]
-export default List;
+export default List;    
